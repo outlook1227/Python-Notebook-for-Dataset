1b0e6a58e9836b2b0d876a94840bd8691460e1a5
Đỗ Đức Tiến <73741677+outlook1227@users.noreply.github.com>
2022-09-27 12:04:47 +0700

    Update Vietnam National High School Graduation Examination.py

--- Vietnam National High School Graduation Examination/Code/Vietnam National High School Graduation Examination.py
+++ Vietnam National High School Graduation Examination/Code/Vietnam National High School Graduation Examination.py
@@ -8,5 +8,43 @@ import numpy as np
 examination_dataset = pd.read_csv("/content/VN_Graduation_Examination_2021.csv")
 examination_dataset
 
-# Check missing, outlier in the dataset
+"Data missing and Data Cleaning"
+# Check missing, outlier, information in the dataset
 examination_dataset.isna().sum()
+examination_dataset.info()
+examination_dataset.describe()
+
+"Visualization for the information of dataset with graph"
+plt.figure(figsize = (20, 15))
+sns.set(font_scale = 1.3)
+sns.heatmap(examination_dataset[["id_examinee","Math", "Physics", "Chemistry", "History", "Biology", "Geography", 
+                        "Literature", "Civic_Education", "English", "German", "Chinese", "Russian", "Japanese", "French"]].corr(), 
+            cmap = "GnBu_r", annot = True)
+plt.title("Heatmap for the dataset of columns")
+plt.show()
+
+# Select the continuous features
+Math = examination_dataset["Math"]
+Physics = examination_dataset["Physics"]
+Chemistry = examination_dataset["Chemistry"]
+Biology = examination_dataset["Biology"]
+History = examination_dataset["History"]
+Geography = examination_dataset["Geography"]
+Literature = examination_dataset["Literature"]
+Civic_Education = examination_dataset["Civic_Education"]
+English = examination_dataset["English"]
+German = examination_dataset["German"]
+Chinese = examination_dataset["Chinese"]
+Russian = examination_dataset["Russian"]
+French = examination_dataset["French"]
+Japanese = examination_dataset["Japanese"]
+
+plt.figure(figsize = (20, 15))
+sns.set(font_scale = 1.4)
+dataset = pd.DataFrame(data = examination_dataset, columns = ["Math", "Physics", "Chemistry", "History", "Biology", "Geography", 
+                        "Literature", "Civic_Education", "English"])
+boxplot_dataset = sns.boxplot(x="variable", y="value", data=pd.melt(dataset), showfliers = False)
+boxplot_dataset.set_xlabel("Subjects")
+boxplot_dataset.set_ylabel("Marks")
+plt.title("Destiny for mark of the Graduation Examination")
+plt.show()
