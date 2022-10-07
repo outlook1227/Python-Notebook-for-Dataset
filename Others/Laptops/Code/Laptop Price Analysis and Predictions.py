2282e294d203b6e70096991846f3cfef30f139c8
Đỗ Đức Tiến <73741677+outlook1227@users.noreply.github.com>
2022-10-07 09:06:52 +0700

    Update Laptop Price Analysis and Predictions.py

--- Others/Laptops/Code/Laptop Price Analysis and Predictions.py
+++ Others/Laptops/Code/Laptop Price Analysis and Predictions.py
@@ -43,13 +43,14 @@ for counts in count_brand.patches:
 
 plt.xlabel("Brand")
 plt.ylabel("Count")
+plt.title("The list of Brand Computer in dataset")
 plt.show()
 
 # 2: Count of the processor band of computer
 plt.figure(figsize = (20, 15))
 sns.set(font_scale = 1.35)
 count_process_brand = sns.countplot(laptop_dataset["processor_brand"])
-count_process_brand.set_xlabel(count_process_brand.get_xticklabels())
+count_process_brand.set_xticklabels(count_process_brand.get_xticklabels())
 
 for counts in count_process_brand.patches:
   bar_height = counts.get_height()
@@ -59,6 +60,7 @@ for counts in count_process_brand.patches:
 
 plt.xlabel("Processor Brand")
 plt.ylabel("Count")
+plt.title("The list of Computer Brand")
 plt.show()
 
 # 3: Count the processor name of computer
@@ -75,4 +77,22 @@ for counts in count_process_name.patches:
 
 plt.xlabel("Processor Name")
 plt.ylabel("Count")
+plt.title("The list of Processor Name in Dataset")
+plt.show()
+
+# 4: Count the processor generations of computer
+plt.figure(figsize = (20, 15))
+sns.set(font_scale = 1.35)
+count_process_gnrtn = sns.countplot(laptop_dataset["processor_gnrtn"])
+count_process_gnrtn.set_xticklabels(count_process_gnrtn.get_xticklabels())
+
+for counts in count_process_gnrtn.patches:
+  bar_height = counts.get_height()
+  label_x = counts.get_x() + counts.get_width() / 2
+  label_y = counts.get_y() / bar_height + bar_height
+  plt.text(label_x, label_y, s = f"{bar_height:}", ha='center', va='bottom', color = "black", size = 15)
+
+plt.xlabel("Processor Generations")
+plt.ylabel("Count")
+plt.title("The list of Processor Generations")
 plt.show()
